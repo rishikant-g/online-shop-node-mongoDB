705f3a1545d93f28ea45ed050d03300b55fa1300
Rishikant <rishikant.npnp@gmail.com>
2020-04-29 14:43:18 +0530

    Added password validation on signup page

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { check } = require('express-validator/check');
+const { check , body } = require('express-validator/check');
 const authController = require('../constrollers/auth/authController');
 
 router.get('/sign-up',authController.getSignUp);
 
-router.post('/sign-up',check('email').isEmail().withMessage('Invalid email format'),authController.postSignUp);
+router.post('/sign-up',
+check('email').isEmail().withMessage('Invalid email format'),
+body('password').isLength({min:5})
+.withMessage('Password atleast 5 digit'),
+authController.postSignUp);
 
 router.get('/login',authController.getLogin);
 
